0e01cf84b920be96f4ab73507cd7ae6bdef5e65c
ptaylor2018 <taylor.patrick@charterschool.org>
2017-05-16 14:15:34 -0400

    add a bunch of shit(it's broken)

--- site/js/script.js
+++ site/js/script.js
@@ -7,14 +7,31 @@ $(document).ready(function(){
     $(".area-link").mouseout(function(){
         $(this).css('font-style', '');
     });
-    $(".are-link").mouseover(function(){
-        $(this).css('font-style', 'italic');
-    });
+
     //scroll down
     $(".down-arrow").click(function() {
         $('html, body').animate({
             scrollTop: $("#about-me").offset().top
         }, 2000);
     });
+    //buttons
+    $(".subsection_button_design").mouseover(function(){
+        $(this).css('background-color', '');
+    });
+    $(".subsection_button_coding").mouseover(function(){
+        $(this).css('background-color', '');
+    });
+    $(".subsection_button_stem").mouseover(function(){
+        $(this).css('background-color', '#eb5936');
+    });
+    $(".subsection_button_music").mouseover(function(){
+        $(this).css('background-color', '');
+    });
+    $(".subsection_button_leadership").mouseover(function(){
+        $(this).css('background-color', '');
+    });
 
+    $(".subsection_button").mouseout(function(){
+        $(this).css('background-color', '#000000');
+    });
 });
